4876da1c11d2075f591bd032eef127024c8281fa
Aleks Vino <aleks.vino@gmail.com>
2018-01-19 20:06:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,7 +32,8 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.pop()
+  array.pop()
+  return array
 }
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.lengh - 1)
